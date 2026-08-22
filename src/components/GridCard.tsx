6eeebc2e8a032d3f344c9a20b13b5f3923880318
Acer-Aspire-3 <1zeydinik@gmail.com>
2026-08-23 01:12:51 +0200

    add HomePage media Queries

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,29 +1,36 @@
-import { TbMovie } from 'react-icons/tb';
-import './GridCard.css';
-import { FaRegBookmark } from 'react-icons/fa';
-
-export default function GridCard() {
-  return (
-    <div className="GridCard">
-      <div className="thumbnail">
-        {/*  <div className="fade"></div> */}
-        <i className="bookmark">
-          <FaRegBookmark className="icon" />
-        </i>
-      </div>
-      <div className="info">
-        <div className="top">
-          <span className="year">2019</span>
-          <span className="bullet">&#8226;</span>
-          <span className="content_type_container">
-            <TbMovie />
-            <span className="content_type">Movie</span>
-          </span>
-          <span className="bullet">&#8226;</span>
-          <span className="category">PG</span>
-        </div>
-        <span className="content_title">Beyond Earth</span>
-      </div>
-    </div>
-  );
-}
+import { TbMovie } from 'react-icons/tb';
+import './GridCard.css';
+import { FaRegBookmark } from 'react-icons/fa';
+import PlayIcon from '../assets/icon-play.svg';
+
+export default function GridCard() {
+  return (
+    <div className="GridCard">
+      <div className="thumbnail">
+        <div className="play_button">
+          <img
+            src={PlayIcon}
+            alt="play button"
+          />
+          <span>Play</span>
+        </div>
+        <i className="bookmark">
+          <FaRegBookmark className="icon" />
+        </i>
+      </div>
+      <div className="info">
+        <div className="top">
+          <span className="year">2019</span>
+          <span className="bullet">&#8226;</span>
+          <span className="content_type_container">
+            <TbMovie />
+            <span className="content_type">Movie</span>
+          </span>
+          <span className="bullet">&#8226;</span>
+          <span className="category">PG</span>
+        </div>
+        <span className="content_title">Beyond Earth</span>
+      </div>
+    </div>
+  );
+}
